539af05d6265a6f82e0d29c7da4408e041775142
Kyle Luzny <kluzny@gmail.com>
2015-08-06 22:56:24 -0500

    trivial example

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # FizzBuzz
 ## implementations
 1. Simplest - each loop over a range, with an if else block
+2. Trivial - an upto loop, using the return value from an if else block
---
+++ fizz_buzz_2.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+1.upto(100) do |i|
+  msg = if i % 15 == 0
+    'fizzbuzz'
+  elsif i % 3 == 0
+    'fizz'
+  elsif i % 5 == 0
+    'buzz'
+  else
+    i
+  end
+  puts msg
+end
